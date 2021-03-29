ef4208cc6c452f89082d5a7fdbb28b8d1d975c9e
Liam Chalcroft <liam.chalcroft.20@ucl.ac.uk>
2021-03-29 15:30:40 +0100

    Add debugging printout

--- nitorch/nn/modules/cnn.py
+++ nitorch/nn/modules/cnn.py
@@ -2280,12 +2280,13 @@ class GroupNet(tnn.Sequential):
 
         modules = []
         for i in range(fusion_depth):
+            print(encoder[i])
             encoder[i] *= in_channels[0]
             groups_encoder[i] *= in_channels[0]
             batch_norm_encoder[i] = tnn.GroupNorm(groups_encoder[i], encoder[i])
             # pool over channels to keep decoder shape consistent
             group_pool = Conv(dim=dim, in_channels=encoder[i], 
-            out_channels=int(encoder[i]//in_channels), kernel_size=1)
+            out_channels=int(encoder[i]//in_channels[0]), kernel_size=1)
             modules.append(('group_pool', group_pool))
         
         enc = Encoder(dim,
