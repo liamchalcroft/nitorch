96e16edbaaa7c2dbd38c81d8047816154a02e8ec
Liam Chalcroft <liam.chalcroft.20@ucl.ac.uk>
2021-05-07 12:26:07 +0100

    Add debug printout for len(decoder)-n

--- nitorch/nn/modules/cnn.py
+++ nitorch/nn/modules/cnn.py
@@ -2362,6 +2362,7 @@ class GroupNet(tnn.Sequential):
         *encoder, bottleneck = encoder
         for n in range(len(decoder) - 1):
             if fusion_depth:
+                print(len(decoder)-n)
                 if (len(decoder)-n) <= fusion_depth:
                     cin = decoder[n] + (encoder[-n - 1] // in_channels)
                 else:
