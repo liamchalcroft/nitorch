4b3c6f996f05cd654a001bdaa8c485e05fafd2fb
Liam Chalcroft <liam.chalcroft.20@ucl.ac.uk>
2021-05-18 11:29:33 +0100

    Convert weights to tuple of weights for groups

--- nitorch/nn/modules/hyper.py
+++ nitorch/nn/modules/hyper.py
@@ -204,6 +204,7 @@ class HyperConv(tnn.Module):
             bias /= len(meta_)
 
         weight = weight.to(device)
+        weight = torch.split(weight, len(meta_batch))
         bias = bias.to(device)
 
         if self.batch_norm:
