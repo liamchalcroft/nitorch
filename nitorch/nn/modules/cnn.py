57822e8a61444a1d1805a8bd30eda2d79f32bde2
Liam Chalcroft <liam.chalcroft.20@ucl.ac.uk>
2021-05-31 14:02:53 +0100

    Add 'flatten' to Discr to remove trailing dims

--- nitorch/nn/modules/cnn.py
+++ nitorch/nn/modules/cnn.py
@@ -1338,7 +1338,7 @@ class Discriminator(tnn.Sequential):
             x = self.red(x)
             out = []
             for head_ in self.head:
-                out.append(head_(x))
+                out.append(head_(x).flatten(start_dim=1))
         return out
 
 
