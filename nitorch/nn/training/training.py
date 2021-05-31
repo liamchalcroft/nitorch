51b63cabf6a9a0aeb21d09746ee13adeca07df02
Liam Chalcroft <liam.chalcroft.20@ucl.ac.uk>
2021-05-31 13:51:33 +0100

    Add correction to wass_gp for multi-output discr

--- nitorch/nn/training/training.py
+++ nitorch/nn/training/training.py
@@ -904,6 +904,8 @@ class SegGANTrainer:
         mix = (real * eps + fake * (1 - eps)).requires_grad_(True)
 
         disc_mix = disc(mix)
+        if isinstance(disc_mix, (list, tuple)):
+            disc_mix = disc[0]
 
         fake_ = torch.ones(disc_mix.shape, requires_grad=False)
         fake_ = fake_.to(device)
