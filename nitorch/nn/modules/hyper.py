d8338780f3165d93d23b147eef23f23374ad210d
Liam Chalcroft <liam.chalcroft.20@ucl.ac.uk>
2021-07-15 11:26:43 +0100

    Add debug print for meta shape

--- nitorch/nn/modules/hyper.py
+++ nitorch/nn/modules/hyper.py
@@ -105,8 +105,10 @@ class HyperGroupNorm(tnn.Module):
 
         for block in self.blocks:
             block = block.to(device)
+            print('Shape in: ', meta.shape)
             meta = block(meta)
             meta = self.meta_act(meta)
+            print('Shape out: ', meta.shape)
 
         weight = self.head_w(meta)
         bias = self.head_b(meta)
