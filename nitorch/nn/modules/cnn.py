86d3cb112fad85336d481643ece659a3855119b3
Liam Chalcroft <liam.chalcroft.20@ucl.ac.uk>
2021-07-24 13:01:17 +0100

    Remove bottleneck pad

--- nitorch/nn/modules/cnn.py
+++ nitorch/nn/modules/cnn.py
@@ -2988,7 +2988,8 @@ class GroupNet(tnn.Sequential):
         pad = self.get_padding(buffers[-1].shape, x.shape, self.bottleneck)
 
         if self.hyper and not isinstance(self.fusion_depth, int):
-            x = self.bottleneck(x, meta=meta, output_padding=pad)
+            # x = self.bottleneck(x, meta=meta, output_padding=pad)
+            x = self.bottleneck(x, meta=meta)
         else:
             x = self.bottleneck(x, output_padding=pad)
         print(x.shape)
