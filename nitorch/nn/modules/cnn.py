c6bd7cfae8af4dbcd5861e1af98979375ca9b7b1
Liam Chalcroft <liam.chalcroft.20@ucl.ac.uk>
2021-08-05 16:11:09 +0100

    Try making stack list explicit

--- nitorch/nn/modules/cnn.py
+++ nitorch/nn/modules/cnn.py
@@ -3346,7 +3346,7 @@ class PhysicsSegNet(tnn.Sequential):
         phys = self.resize_phys(phys, x)
         buffer = buffers.pop()
         print(x.shape, buffer.shape, phys.shape)
-        x = self.stack(x, buffers, phys)
+        x = self.stack([x, buffers, phys])
         f = x if return_feat else None
         x = self.final(x)
         return (x, f) if return_feat else x
